7207e06d78155644139bceb6ea757b6b517b7c08
dinkybluebug <github@peacocksmith.co.uk>
2016-11-20 16:23:01 +0000

    Initial upload

---
+++ normal.sh
@@ -0,0 +1,3 @@
+cp ../MagicMirror/css/normal_custom.css ../MagicMirror/css/custom.css
+cp ../MagicMirror/config/normal_config.js ../MagicMirror/config/config.js
+pm2 restart all
---
+++ party.sh
@@ -0,0 +1,3 @@
+cp ../MagicMirror/css/party_custom.css ../MagicMirror/css/custom.css
+cp ../MagicMirror/config/party_config.js ../MagicMirror/config/config.js
+pm2 restart all
